bd3fa6dded61c978666d7c6d3c34863d49a35c3e
Pascal Schneider <96p.schneider@gmail.com>
2026-02-11 08:47:41 +0100

    conflict resolution and dictionaries

--- functions/_source.R
+++ functions/_source.R
@@ -1,3 +1,6 @@
+
+# Packages ------------------------------------------------------------------------------------
+
 library(tidyverse)
 library(ggplot2)
 library(ggalluvial)
@@ -7,22 +10,36 @@ library(janitor)
 library(ggpubr)
 library(ggpattern)
 library(rlang)
+library(dplyr)
+library(conflicted)
+
+
+
+# Conflicts -----------------------------------------------------------------------------------
+
+conflicts_prefer(
+  dplyr::select,
+  dplyr::filter,
+  dplyr::mutate
+)
+
+# Manual Functions ----------------------------------------------------------------------------
 
 # Main Functions
 get_meta <- function(which = c("tree", "box")) {
   if (which == "tree") {
-    return(
-      left_join(
+    df <- left_join(
         read_csv("./data/interim/meta_tree.csv",show_col_types = FALSE),
         read_csv("./data/interim/meta_box.csv",show_col_types = FALSE),
         by="boxlabel"
-      )
-    )
+      ) |> 
+      mutate(tree_id = as.character(tree_id))
   } else if (which == "box") {
-    return(read_csv("./data/interim/meta_box.csv", show_col_types = FALSE))
+    df <- read_csv("./data/interim/meta_box.csv", show_col_types = FALSE)
   } else {
     stop("Invalid 'which' argument. Use 'tree' or 'box'.")
   }
+  return(df)
 }
 
 get_data <- function(type = c("tree", "box"), data_name, with_meta = TRUE, path = "./data/interim") {
@@ -68,13 +85,107 @@ get_data <- function(type = c("tree", "box"), data_name, with_meta = TRUE, path 
   # Optionally attach meta
   if (with_meta) {
     if (type == "tree"){
-      df <- df %>% left_join(get_meta("tree"), by="tree_id")
+      df <- df |>
+        mutate(tree_id = as.character(tree_id)) |> 
+        left_join(get_meta("tree"), by="tree_id")
     } else {
-      df <- df %>% left_join(get_meta("box"), by="boxlabel")
+      df <- df |>
+        left_join(get_meta("box"), by="boxlabel")
     }
+  }
+  
+  # Attach extreme event information
+  buffer_days <- 14  # 3 weeks (to cover measurement measured after the extreme event)
+  
+  df <- df |> 
+    mutate(
+      extreme_event =
+        dplyr::between(
+          date,
+          as.Date("2025-06-20"),
+          as.Date("2025-07-03") + buffer_days
+        ) |
+        dplyr::between(
+          date,
+          as.Date("2025-08-12"),
+          as.Date("2025-08-20") + buffer_days
+        ),
+      extreme_event = if_else(extreme_event, "yes", "no")
+    )
+  
+  # Attach date_num 
+  df <- df |> 
+    mutate(date_num = as.numeric(date))
+  
+  # Attach closes or latest SWC data
+  if (type == "tree") {
     
+    df_swc <- get_data("box", "soilwater")
+    
+    df <- df %>%
+      group_by(boxlabel) %>%
+      mutate(
+        swc = sapply(date, function(d) {
+          # restrict to current box
+          box <- unique(boxlabel)
+          idx_box <- which(df_swc$boxlabel == box)
+          if (length(idx_box) == 0) return(NA_real_)
+          
+          swc_box <- df_swc[idx_box, ]
+          swc_box <- swc_box[order(swc_box$date), ]
+          
+          d_minus7 <- d - 7
+          d_plus7  <- d + 7
+          
+          # 1) closest SWC within -7 days (past week, including same day)
+          idx1 <- which(swc_box$date <= d & swc_box$date >= d_minus7)
+          if (length(idx1) > 0) {
+            # latest date in that interval
+            i <- idx1[which.max(swc_box$date[idx1])]
+            return(swc_box$swc[i])
+          }
+          
+          # 2) if none, closest within +7 days (next week)
+          idx2 <- which(swc_box$date > d & swc_box$date <= d_plus7)
+          if (length(idx2) > 0) {
+            # earliest date after d
+            i <- idx2[which.min(swc_box$date[idx2])]
+            return(swc_box$swc[i])
+          }
+          
+          # 3) if still none, closest back in time (unbounded, as before)
+          idx3 <- which(swc_box$date <= d)
+          if (length(idx3) > 0) {
+            i <- idx3[which.max(swc_box$date[idx3])]
+            return(swc_box$swc[i])
+          }
+          
+          # if there is no SWC at all before/after
+          NA_real_
+        })
+      ) %>%
+      ungroup()
   }
   
   message("✅ Loaded ", data_file, if (with_meta) paste0(" with ", meta_file) else "")
-  df
+  return(df)
+}
+
+
+get_data_var_grid <- function(){
+  tibble::tribble(
+    ~type,   ~data,          ~resp_var,
+    "tree",  "chlorophyll",  "chl",
+    "tree",  "condition",    "condition",
+    "tree",  "growth",       "height",
+    "tree",  "growth",       "diameter",
+    # "tree",  "growth",       "xxx_inc_t0",
+    # "tree",  "growth",       "xxx_inc_t0_rel",
+    "tree",  "phenology",    "stage",
+    "tree",  "senescence",   "percent_senesced",
+    "tree",  "senescence",   "chlavg",
+    "tree",  "quantum_yield","qy",
+    "box",   "respiration",  "co2",
+    "box",   "soilwater",    "swc"
+  )
 }
